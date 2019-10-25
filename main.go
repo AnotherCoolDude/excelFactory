fcacc64460d7a92bc47d659cfe778f23ec3908b9
Christian Hovenbitzer <christian.hovenbitzer@gmx.net>
2019-10-25 14:48:51 +0200

    working on it

--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"github.com/AnotherCoolDude/excelFactory/excelfactory"
+	"strconv"
+)
 
 const (
 	filepath = "/Users/christianhovenbitzer/Desktop/rentabilität01-10.xlsx"
 )
 
 func main() {
-
+	f, err := excelfactory.ReadFile(filepath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	rows, err := f.Sheets[0].FilterRowsByColumn("B", func(value string) bool {
+		return value == "ABCH"
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	revenue := 0.0
+	for _, row := range rows {
+		for col, cell := range row {
+			if col != 11 {
+				continue
+			}
+			amount, err := strconv.ParseFloat(cell, 64)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			revenue += amount
+		}
+	}
+	// todo put revenue into ob
 }
